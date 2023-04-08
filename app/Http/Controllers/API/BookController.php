5b7bc1d08143325b9f19633945594f2d99b0ec22
Dev-Ismael <dev.ismael305@gmail.com>
2023-04-08 15:10:14 +0200

    Add API controller& model & migrateion

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+        return response()->json(['data' => $books]);
+    }
+
+    public function store(Request $request)
+    {
+        $book = Book::create($request->all());
+        return response()->json(['data' => $book], 201);
+    }
+
+    public function show($id)
+    {
+        $book = Book::find($id);
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+        return response()->json(['data' => $book]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $book = Book::find($id);
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+        $book->update($request->all());
+        return response()->json(['data' => $book]);
+    }
+
+    public function destroy($id)
+    {
+        $book = Book::find($id);
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+        $book->delete();
+        return response()->json(['message' => 'Book deleted'], 204);
+    }
+}
